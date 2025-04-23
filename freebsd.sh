d434a92d93ba72ce3e8ed60e062999bc4ccda49d
bahaapix <bahaa13pix@gmail.com>
2025-04-23 08:55:50 +0100

    Update freebsd.sh

--- freebsd.sh
+++ freebsd.sh
@@ -1,3 +1,3 @@
-su - root -c 'sed -i "" "s/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers'
+su - root -c 'pkg install -y sudo && sed -i "" "s/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers'
 chmod +x setup.sh
 sh setup.sh
