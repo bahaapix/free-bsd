c1df1eda5c8af9680691e4d11c2a5f97caa64693
bahaapix <bahaa13pix@gmail.com>
2025-04-23 08:56:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo pkg install -y drm-kmod
 sudo sysrc kld_list+=i915kms
-sudo pkg install -y vim fastfetch xorg alacritty dmenu dwm chromium
+sudo pkg install -y vim fastfetch xorg alacritty dmenu dwm chromium pulseaudio pavucontrol
 echo 'exec dwm' > ~/.xinitrc
